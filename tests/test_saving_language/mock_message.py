0590378947e90ea76e7d43e227b1388a45f665a6
kipariss1 <kpassudin@gmail.com>
2025-02-08 18:14:22 +0100

    added Message

--- tests/test_saving_language/mock_message.py
+++ tests/test_saving_language/mock_message.py
@@ -1,6 +1,16 @@
+from dataclasses import dataclass, field
+from typing import Any, Dict
+
+@dataclass
 class Message:
-    def __init__(self, data: dict):
-        pass
+    data: Dict[str, Any] = field(default_factory=dict)
+
+    def __post_init__(self):
+        for key, value in self.data.items():
+            if isinstance(value, dict):
+                object.__setattr__(self, key, DictToObject(value))
+            else:
+                object.__setattr__(self, key, value)
 
     def __getattr__(self, item):
-        pass
+        return self.data.get(item, None)
