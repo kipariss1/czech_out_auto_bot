61e6bd3bafc2eb8ccf4e49d664231f7bbd273c61
kipariss1 <kpassudin@gmail.com>
2025-02-09 13:48:50 +0100

    fixed tests

--- tests/test_saving_language/test_saving_language_to_db.py
+++ tests/test_saving_language/test_saving_language_to_db.py
@@ -6,15 +6,14 @@ from run_bot import start, set_users_language, get_or_create_user
 
 @pytest.fixture
 def mock_bot(monkeypatch) -> MockBot:
-    mb = 1  # MockBot()
+    mb = MockBot()
     return mb
 
 
 def test_language_saved_to_db(mock_bot):
     user_id = 3156
-    # mock_bot.add_handler(start, {"message": StartMessage(user_id=user_id)})
-    # mock_bot.add_handler(set_users_language, {"message": LangMessage(user_id=user_id)})
-    # mock_bot.infinity_polling()
-    # user = get_or_create_user(user_id)
-    # assert user.language == "ru"
-    pass
+    mock_bot.add_handler(start, {"message": StartMessage(user_id=user_id)})
+    mock_bot.add_handler(set_users_language, {"message": LangMessage(user_id=user_id)})
+    mock_bot.infinity_polling()
+    user = get_or_create_user(user_id)
+    assert user.language == "ru"
