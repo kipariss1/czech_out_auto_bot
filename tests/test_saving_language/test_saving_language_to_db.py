4e6547ba5dd1ccd3b0f67f6a87dc3dd0c978a2f4
kipariss1 <kpassudin@gmail.com>
2025-02-09 16:25:45 +0100

    finished with tests

--- tests/test_saving_language/test_saving_language_to_db.py
+++ tests/test_saving_language/test_saving_language_to_db.py
@@ -1,19 +1,40 @@
 import pytest
 from tests.test_saving_language.mock_bot import MockBot
-from tests.test_saving_language.mock_message import StartMessage, LangMessage
-from run_bot import start, set_users_language, get_or_create_user
+from unittest.mock import MagicMock
+import run_bot
 
 
 @pytest.fixture
 def mock_bot(monkeypatch) -> MockBot:
     mb = MockBot()
-    return mb
+    monkeypatch.setattr(run_bot, "bot", mb)
+    yield mb
 
 
-def test_language_saved_to_db(mock_bot):
-    user_id = 3156
-    mock_bot.add_handler(start, {"message": StartMessage(user_id=user_id)})
-    mock_bot.add_handler(set_users_language, {"message": LangMessage(user_id=user_id)})
+@pytest.fixture
+def user_id():
+    return 3125
+
+
+@pytest.fixture
+def start_message(user_id):
+    message = MagicMock()
+    message.text = "/start"
+    message.from_user.id = user_id
+    return message
+
+
+@pytest.fixture
+def ru_lang_message(user_id):
+    message = MagicMock()
+    message.text = "/ru"
+    message.from_user.id = user_id
+    return message
+
+
+def test_language_saved_to_db(mock_bot, start_message, ru_lang_message, user_id):
+    mock_bot.add_handler(run_bot.start, {"message": start_message})
+    mock_bot.add_handler(run_bot.set_users_language, {"message": ru_lang_message})
     mock_bot.infinity_polling()
-    user = get_or_create_user(user_id)
+    user = run_bot.get_or_create_user(user_id)
     assert user.language == "ru"
