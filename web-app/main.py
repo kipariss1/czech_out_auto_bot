de1b37287168872f7fc16a37ac9a135a115dbf26
kipariss1 <kpassudin@gmail.com>
2025-02-11 22:03:56 +0100

    theoretically base web app should be ready

--- web-app/main.py
+++ web-app/main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from src import sqlite_db_handler
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="src/web-app/static"), name="static")
+templates = Jinja2Templates(directory="src/web-app/templates")
+
 
 @app.get("/", response_class=HTMLResponse)
-def main_view():
-    pass
+def main_view(request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/create_search", response_class=HTMLResponse)
+def create_search_view(request):
+    return templates.TemplateResponse("create_search.html", {"request": request})
 
 
 if __name__ == "__main__":
