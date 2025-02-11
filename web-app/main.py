09161695aa1609650a4cd8e922475e366c38e96f
kipariss1 <kpassudin@gmail.com>
2025-02-11 22:39:38 +0100

    alomost done with basic web app

--- web-app/main.py
+++ web-app/main.py
@@ -2,25 +2,26 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from starlette.requests import Request
 from src import sqlite_db_handler
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="src/web-app/static"), name="static")
-templates = Jinja2Templates(directory="src/web-app/templates")
+app.mount("/static", StaticFiles(directory="web-app/static"), name="static")
+templates = Jinja2Templates(directory="web-app/templates")
 
 
 @app.get("/", response_class=HTMLResponse)
-def main_view(request):
+def main_view(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.get("/create_search", response_class=HTMLResponse)
-def create_search_view(request):
+def create_search_view(request: Request):
     return templates.TemplateResponse("create_search.html", {"request": request})
 
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, "0.0.0.0", port=8000)
+    uvicorn.run(app=app, host="0.0.0.0", port=8000)
