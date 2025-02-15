297cbd8764401664b1c5f49e28b3d817245901f1
kipariss1 <kpassudin@gmail.com>
2025-02-15 23:20:25 +0100

    almoust done with async scrapping

--- src/scrap_car_models.py
+++ src/scrap_car_models.py
@@ -2,8 +2,22 @@ from src import sqlite_db_handler
 from src.models.models import CarModel
 import requests
 from bs4 import BeautifulSoup
+import asyncio
 
-# TODO: do this assyncronous
+
+async def request_for_id(id: str, car_manufacturer: str):
+    url = f"https://dummy_url/{id}"
+    response = await asyncio.to_thread(requests.get, url)
+    if response.status_code == 200:
+        return {car_manufacturer: response.json()}
+    return None
+
+
+async def gather_and_request(id_list: list):
+    tasks = [request_for_id(id.attrs["value"], id.contents[0]) for id in id_list]
+    return await asyncio.gather(*tasks)
+
+
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 }
@@ -12,4 +26,8 @@ if mobile_de_page.status_code == 200:
     mobile_de_bs = BeautifulSoup(mobile_de_page.text, "html.parser")
 
     select_widget = mobile_de_bs.find("div", class_="W885w")
-    pass
+    if select_widget:
+        car_manufacturer_list = select_widget.find("optgroup", {"label": "All makes"})
+        car_models = gather_and_request(car_manufacturer_list.contents)
+        # TODO: save results in the database
+        pass
