89294a3d4121a6d6be97bc14bc085c8753400f20
kipariss1 <kpassudin@gmail.com>
2025-02-15 22:35:41 +0100

    added headers to scrapping

--- src/scrap_car_models.py
+++ src/scrap_car_models.py
@@ -3,8 +3,11 @@ from src.models.models import CarModel
 import requests
 from bs4 import BeautifulSoup
 
-# TODO: do this assincronous
-mobile_de_page = requests.get("https://mobile.de/?lang=en")
+# TODO: do this assyncronous
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+}
+mobile_de_page = requests.get("https://mobile.de/?lang=en", headers=headers)
 if mobile_de_page.status_code == 200:
     mobile_de_bs = BeautifulSoup(mobile_de_page.text, "html.parser")
 
