8c2a5a479bdd19265fd0d8b611550679f7f4bdc2
kipariss1 <kpassudin@gmail.com>
2025-02-23 14:50:38 +0100

    added model of CarSearch

--- src/models/models.py
+++ src/models/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, Enum, Text
+from sqlalchemy import Column, Integer, Enum, Text, ForeignKey, JSON
 
 Base = declarative_base()
 
@@ -14,8 +14,18 @@ class User(Base):
 
 class CarModel(Base):
 
-    __tablename__ = "Car model"
+    __tablename__ = "Car_Models"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     manufacturer = Column(Text(length=20))
     model = Column(Text(length=40))
+
+
+class CarSearch(Base):
+
+    __tablename__ = "Car_Searches"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("Users.id"), nullable=False)
+    car = Column(Text(length=100), nullable=False)
+    attributes = Column(JSON, nullable=True)
