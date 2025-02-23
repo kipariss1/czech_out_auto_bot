0080f367f351276d76c6a132cdd519b02adf4ae5
kipariss1 <kpassudin@gmail.com>
2025-02-23 16:25:10 +0100

    start to seriously do create_search page

--- web-app/main.py
+++ web-app/main.py
@@ -3,7 +3,9 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from starlette.requests import Request
+from sqlalchemy import distinct
 from src import sqlite_db_handler
+from src.models.models import CarModel
 
 app = FastAPI()
 
@@ -18,7 +20,11 @@ def main_view(request: Request):
 
 @app.get("/create_search", response_class=HTMLResponse)
 def create_search_view(request: Request):
-    return templates.TemplateResponse("create_search.html", {"request": request})
+    db = sqlite_db_handler.get_db_connection()
+    unique_car_manufacturers = db.query(distinct(CarModel.manufacturer)).all()
+    unique_car_manufacturers = list(map(lambda el: el[0], unique_car_manufacturers))
+    render_dict = {"request": request, "car_manufacturers": unique_car_manufacturers}
+    return templates.TemplateResponse("create_search.html", render_dict)
 
 
 if __name__ == "__main__":
