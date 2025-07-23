8deaa9600178fdb2f41a1099d98bc3e6e8944450
kipariss1 <kpassudin@gmail.com>
2025-07-23 22:55:30 +0200

    TODO: запустить бота && посмотреть что с bootstrap на render

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -6,8 +6,8 @@ from pathlib import Path
 class Settings(BaseSettings):
     ENV: str = os.getenv("ENV", "production")
     SETTINGS_DIR: Path = Path(__file__).parent
-    DATABASE_URL: str = f"sqlite:////{SETTINGS_DIR.parent / 'db' / 'local.db'}"
-    TEST_DATABASE_URL: str = f"sqlite:////{SETTINGS_DIR.parent / 'db' / 'test_local.db'}"
+    DATABASE_URL: str = f"sqlite:////{(SETTINGS_DIR.parent / 'db' / 'local.db').as_posix()}"
+    TEST_DATABASE_URL: str = f"sqlite:////{(SETTINGS_DIR.parent / 'db' / 'test_local.db').as_posix()}"
     WEBAPP_BASE_URL: str = "https://czech-out-auto-bot-webapp.onrender.com"    # TODO: change later to env
 
     @property
