a2ca60f460f5f25da1ba24fdfce70cc05d8ab961
kipariss1 <kpassudin@gmail.com>
2025-07-26 22:18:51 +0200

    добавил ssl сертификат

--- web_app/main.py
+++ web_app/main.py
@@ -1,6 +1,6 @@
+import ssl
 from pathlib import Path
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 
 from web_app.endpoints import router
@@ -15,4 +15,10 @@ app.mount("/static", StaticFiles(directory= BASE_DIR / "static"), name="static")
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app=app, host="0.0.0.0", port=8000)
+    uvicorn.run(
+        app=app, 
+        host="0.0.0.0", 
+        port=8000, 
+        ssl_keyfile=BASE_DIR.parent / "certs" / "ssl.key", 
+        ssl_certfile=BASE_DIR.parent / "certs" / "ssl.crt"
+        )
