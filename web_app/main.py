abc7a0326ced90b9134405281be8401d13169b10
kipariss1 <kpassudin@gmail.com>
2025-07-26 22:29:23 +0200

    bootstrap and nouislider is fixed

--- web_app/main.py
+++ web_app/main.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     uvicorn.run(
         app=app, 
         host="0.0.0.0", 
-        port=8000, 
-        ssl_keyfile=BASE_DIR.parent / "certs" / "ssl.key", 
-        ssl_certfile=BASE_DIR.parent / "certs" / "ssl.crt"
-        )
+        port=8000,
+        proxy_headers=True,
+        forwarded_allow_ips="*"
+    )
