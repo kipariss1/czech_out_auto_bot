b7135f639e86d91dc285b0b5ffc58a256483b517
kipariss1 <kpassudin@gmail.com>
2025-07-31 18:24:19 +0200

    fixed db is not created error on Win and Linux

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import platform
 
 SRC_DIR = Path(__file__).parent
+OS_NAME = platform.system()
 
--- src/settings/settings.py
+++ src/settings/settings.py
@@ -1,12 +1,12 @@
 import os
 from pydantic_settings import BaseSettings
-from src import SRC_DIR
+from src import SRC_DIR, OS_NAME
 
 
 class Settings(BaseSettings):
     ENV: str = os.getenv("ENV", "production")
-    DATABASE_URL: str = f"sqlite:///./src/db/local.db"
-    TEST_DATABASE_URL: str = f"sqlite:///./src/db/test.db"
+    DATABASE_URL: str = f"sqlite:////{(SRC_DIR / 'db' / 'local.db').as_posix()}" if OS_NAME == "Linux" else f"sqlite:///./src/db/local.db"
+    TEST_DATABASE_URL: str = f"sqlite:////{(SRC_DIR / 'db' / 'test_local.db').as_posix()}" if OS_NAME == "Linux" else f"sqlite:///./src/db/test_local.db"
     WEBAPP_BASE_URL: str = os.getenv("RENDER_EXTERNAL_URL")
 
     @property
