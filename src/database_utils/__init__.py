0edb95129e68d9be842d6bb8aec86d79adb3c4e0
kipariss1 <kpassudin@gmail.com>
2025-08-25 12:56:17 +0200

    everything starts all at once

--- src/database_utils/__init__.py
+++ src/database_utils/__init__.py
@@ -1,4 +1,3 @@
 from src.database_utils.database_factory import DBFactory
 
 db_handler = DBFactory.create_db_handler()
-db_handler.updload_cars_to_db()
--- src/database_utils/init_db.py
+++ src/database_utils/init_db.py
@@ -1,4 +1,4 @@
-from src.database_utils.database_factory import DBFactory
+from src.database_utils import db_handler
 
-db_handler = DBFactory.create_db_handler()
 db_handler.create_tables()
+db_handler.updload_cars_to_db()
