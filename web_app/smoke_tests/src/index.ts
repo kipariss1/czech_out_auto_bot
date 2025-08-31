a2d2fb15e9ab01d2f391764264ff4dd505851592
kipariss1 <kpassudin@gmail.com>
2025-08-31 18:51:02 +0200

    added infrastructure to tests

--- web_app/smoke_tests/src/index.ts
+++ web_app/smoke_tests/src/index.ts
@@ -1,6 +1,6 @@
 import path from "path"
 import { SQLiteDBhandler } from "./db/SQLiteDBhandler.js"
-import { CipherHandler } from './security/cipherHandler.js';
+import { CipherHandler } from './security/CipherHandler.js';
 
 export const smokeTestsDir = path.dirname(path.dirname(__filename));
 export const sqliteDBhandler = new SQLiteDBhandler();
