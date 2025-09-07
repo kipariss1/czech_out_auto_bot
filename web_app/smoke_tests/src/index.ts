900327f8c9c4f1c47e569448cf3a5f9ba2a894c1
kipariss1 <kpassudin@gmail.com>
2025-09-08 00:22:06 +0200

    updated import CipherHandler -> cipherHandler

--- web_app/smoke_tests/src/index.ts
+++ web_app/smoke_tests/src/index.ts
@@ -1,6 +1,6 @@
 import path from "path"
 import { SQLiteDBhandler } from "./db/SQLiteDBhandler.js"
-import { CipherHandler } from './security/CipherHandler.js';
+import { CipherHandler } from './security/cipherHandler.js';
 import { fileURLToPath } from "url";
 
 export const smokeTestsDir = path.dirname(path.dirname(fileURLToPath(import.meta.url)));
