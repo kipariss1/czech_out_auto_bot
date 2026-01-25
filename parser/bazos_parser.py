3c2863977341229e35fa29180755bbd3482527de
kipariss1 <kpassudin@gmail.com>
2026-01-25 15:54:54 +0100

    finished parser
    TODO: write tests

--- parser/bazos_parser.py
+++ parser/bazos_parser.py
@@ -26,13 +26,45 @@ class BazosParser:
         )
         return min_from, max_to
     
-    def _get_last_searched_id(self, car_id: int) -> int | None:
-        stmt = select(CarModel.last_checked_id).where(CarModel.id == car_id)
-        last_checked_id = self.db.execute(stmt).scalar_one_or_none()
-        return last_checked_id
+    def _go_to_last_checked_id(self, car: CarModel, car_page_bazos: AutoPage) -> tuple[str | None, list[AutoAdvertisementPage]]:
+        last_checked_id = car.last_checked_id
+        car_ads = car_page_bazos.get_advertisements()
+        car_ads = list(map(lambda ad: AutoAdvertisementPage(ad), car_ads))
+        car_ads_ids = [e.id for e in car_ads]
+        while (last_checked_id and last_checked_id not in car_ads_ids):
+            car_page_bazos.go_next_page()
+            car_ads = car_page_bazos.get_advertisements()
+            car_ads = list(map(lambda ad: AutoAdvertisementPage(ad), car_ads))
+            car_ads_ids = [e.id for e in car_ads]
+        page_with_last_checked_id = car_ads
+        return last_checked_id, page_with_last_checked_id
+    
+    def _form_queue_to_check(self, car_page_bazos: AutoPage, last_checked_id: str | None, page_with_last_checked_id: list[AutoAdvertisementPage]) -> list[AutoAdvertisementPage]:
+        if not last_checked_id: 
+                queue_to_check = page_with_last_checked_id
+        else:
+            idx_of_last_checked_add = next(i for i, e in enumerate(page_with_last_checked_id) if e.id == last_checked_id)
+            queue_to_check = page_with_last_checked_id[:idx_of_last_checked_add]
+            while (car_page_bazos.go_previous_page()):
+                car_ads = car_page_bazos.get_advertisements()
+                car_ads = list(map(lambda ad: AutoAdvertisementPage(ad), car_ads))
+                queue_to_check.append(car_ads)
+        return queue_to_check
+    
+    def _add_queue_to_db(self, car_id: str, queue_to_check: list[AutoAdvertisementPage]):
+        links = [e.link for e in queue_to_check]
+        row = self.db.query(AdQueue).filter_by(car_model_id=car_id).one_or_none()
+        if row:
+            row.queue = links
+        else:
+            row = AdQueue(
+                car_model_id=car_id,
+                queue=links
+            )
+            self.db.add(row)
+        self.db.commit()
         
     async def parse(self):
-        # TODO: refactor this gigantic function to be many small ones
         unique_car_searches_query = select(distinct(CarSearch.car_model_id))
         unique_car_ids = self.db.execute(unique_car_searches_query).scalars().all()
         for car_id in unique_car_ids:
@@ -45,38 +77,9 @@ class BazosParser:
                 'price_from': min_from,
                 'price_to': max_to
             }
-            # FINDING LAST CHECKED AD
-            last_checked_id = self._get_last_searched_id(car_id)
             car_page_bazos = AutoPage(**args)
-            car_ads = car_page_bazos.get_advertisements()
-            car_ads = list(map(lambda ad: AutoAdvertisementPage(ad), car_ads))
-            car_ads_ids = [e.id for e in car_ads]
-            while (last_checked_id and last_checked_id not in car_ads_ids):
-                car_page_bazos.go_next_page()
-                car_ads = car_page_bazos.get_advertisements()
-                car_ads = list(map(lambda ad: AutoAdvertisementPage(ad), car_ads))
-                car_ads_ids = [e.id for e in car_ads]
-            # PARCING ALL THE NEW ADDS
-            if not last_checked_id: 
-                queue_to_check = car_ads
-            else:
-                idx_of_last_checked_add = next(i for i, e in enumerate(car_ads) if e.id == last_checked_id)
-                queue_to_check = car_ads[:idx_of_last_checked_add]
-                while (car_page_bazos.go_previous_page()):
-                    car_ads = car_page_bazos.get_advertisements()
-                    car_ads = list(map(lambda ad: AutoAdvertisementPage(ad), car_ads))
-                    queue_to_check.append(car_ads)
-            # ADDING ADDS TO QUEUE
-            links = [e.link for e in queue_to_check]
-            row = self.db.query(AdQueue).filter_by(car_model_id=car_id).one_or_none()
-            if row:
-                row.queue = links
-            else:
-                row = AdQueue(
-                    car_model_id=car_id,
-                    queue=links
-                )
-                self.db.add(row)
-            self.db.commit()
+            last_checked_id, page_with_last_checked_id = self._go_to_last_checked_id(car, car_page_bazos)
+            queue_to_check = self._form_queue_to_check(car_page_bazos, last_checked_id, page_with_last_checked_id)
+            self._add_queue_to_db(car_id, queue_to_check)            
 
 
