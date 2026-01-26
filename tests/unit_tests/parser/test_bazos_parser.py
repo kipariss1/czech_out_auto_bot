b9fdc5ec64c2507d3b2ddab5f94d58829eff0d7d
kipariss1 <kpassudin@gmail.com>
2026-01-26 23:11:21 +0100

    drafts of test

--- tests/unit_tests/parser/test_bazos_parser.py
+++ tests/unit_tests/parser/test_bazos_parser.py
@@ -1,3 +1,11 @@
 from parser.bazos_parser import BazosParser
+import pytest
 
-pass
+
+def test_bazos_parser_correctly_finds_the_last_checked_ad():
+    pass
+
+def test_bazos_parser_doesnt_lose_older_ads_if_price_margins_changed():
+    pass
+
+
