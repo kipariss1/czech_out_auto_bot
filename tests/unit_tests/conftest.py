581c356976e186f56bf664c1ff31edd06f27a1e0
kipariss1 <kpassudin@gmail.com>
2026-01-26 23:29:28 +0100

    added draft of mock db

--- tests/unit_tests/conftest.py
+++ tests/unit_tests/conftest.py
@@ -1,1 +1,12 @@
-pass
+import pytest
+from unittest.mock import MagicMock, patch
+
+
+@pytest.fixture(scope='function')
+def build_mock_db():
+    def factory(path, tables):
+        mock_db = MagicMock()
+        # TODO: здесь прокинуть данные в тестовую дб
+        with patch(path, return_value=mock_db): 
+            return mock_db
+    return factory
