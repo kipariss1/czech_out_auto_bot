f8a449af55df33b72ce367485e9146db20bd46a9
kipariss1 <kpassudin@gmail.com>
2026-01-28 23:14:40 +0100

    added first draft of test and the configuration

--- parser/main.py
+++ parser/main.py
@@ -1,6 +1,7 @@
 from parser import bp
 import asyncio
 
+# TODO: make a CLI from this
 
 if __name__ == '__main__':
     asyncio.run(bp.parse())
--- tests/unit_tests/parser/test_bazos_parser.py
+++ tests/unit_tests/parser/test_bazos_parser.py
@@ -1,9 +1,22 @@
 from parser.bazos_parser import BazosParser
 import pytest
+import json
 
 
-def test_bazos_parser_correctly_finds_the_last_checked_ad():
-    pass
+def test_bazos_parser_correctly_finds_the_last_checked_ad(build_mock_db):
+    f = open("../test_data/test_bazos_parser_correctly_finds_the_last_checked_ad.json")
+    text = f.read()
+    f.close()
+    mock_data = json.loads(text)
+    mock_db = build_mock_db(
+        "parser.bazos_parser.db_handler.get_db_connection",
+        mock_data
+    )
+    bp = BazosParser()
+    bp.parse()
+
+
+
 
 def test_bazos_parser_doesnt_lose_older_ads_if_price_margins_changed():
     pass
