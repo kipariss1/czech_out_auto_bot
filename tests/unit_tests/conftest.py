8931d258d64bbd6b11dc486f142c40e896f04980
kipariss1 <kpassudin@gmail.com>
2026-01-28 22:35:05 +0100

    updated build_mock_db fxtr to accept data in json

--- tests/unit_tests/conftest.py
+++ tests/unit_tests/conftest.py
@@ -1,12 +1,22 @@
 import pytest
 from unittest.mock import MagicMock, patch
+from typing import Any, Dict, Callable
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy import create_engine
+from src.models.models import Base
 
+JSON = Dict[str, Any]
 
 @pytest.fixture(scope='function')
-def build_mock_db():
-    def factory(path, tables):
-        mock_db = MagicMock()
-        # TODO: здесь прокинуть данные в тестовую дб
+def build_mock_db() -> Callable[[str, JSON], Session]:
+    def factory(path: str, data: JSON) -> Session:
+        engine = create_engine("sqlite:///:memory:")
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        mock_db = Session()
+        for table_name, rows in data.items():
+            model = Base.metadata.tables[table_name]
+            mock_db.execute(model.insert(), rows)
         with patch(path, return_value=mock_db): 
             return mock_db
     return factory
