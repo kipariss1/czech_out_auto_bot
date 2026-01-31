1d81f30ee3e691df183bb61d407415fcc68307fb
kipariss1 <kpassudin@gmail.com>
2026-01-31 18:41:41 +0100

    fixed name

--- tests/unit_tests/parser/test_bazos_parser.py
+++ tests/unit_tests/parser/test_bazos_parser.py
@@ -21,7 +21,7 @@ def test_bazos_parser_correctly_finds_the_last_checked_ad(build_mock_db):
 def test_bazos_parser_doesnt_lose_older_ads_if_price_margins_changed():
     pass
 
-def bazos_parser_correctly_sets_an_lower_price_range():
+def test_bazos_parser_correctly_sets_an_lower_price_range():
     # 10% из верхней границы если нижняя не установлена
     pass
 
