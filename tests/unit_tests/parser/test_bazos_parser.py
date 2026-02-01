2fa574a8f09a98e429f081483ac0fa76bdc2e938
kipariss1 <kpassudin@gmail.com>
2026-02-01 23:38:24 +0100

    removed test for 10% of max_price logic

--- tests/unit_tests/parser/test_bazos_parser.py
+++ tests/unit_tests/parser/test_bazos_parser.py
@@ -84,8 +84,4 @@ def test_bazos_parser_correctly_finds_the_last_checked_ad(build_mock_db, mock_ba
 def test_bazos_parser_doesnt_lose_older_ads_if_price_margins_changed():
     pass
 
-def test_bazos_parser_correctly_sets_an_lower_price_range():
-    # 10% из верхней границы если нижняя не установлена
-    pass
-
 
