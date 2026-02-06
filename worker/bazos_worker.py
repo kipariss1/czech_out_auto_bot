743e0eefd1c1d76bfb222382a06c6e4a36e51b66
kipariss1 <kpassudin@gmail.com>
2026-02-06 11:50:54 +0100

    wrote draft of worker

--- worker/bazos_worker.py
+++ worker/bazos_worker.py
@@ -1,9 +1,22 @@
 from src.database_utils import db_handler
+from src.models.models import AdQueue
+from parser.bazos_api.auto_bazos_api import AutoAdvertisementPage
+import asyncio
 
 class BazosWorker:
     
     def __init__(self):
         self.db = db_handler.get_db_connection()
 
-    def process_queue(self):
-        pass
+    async def process_queue(self):
+        queue_rows = (
+            self.db.query(AdQueue)
+            .filter(AdQueue.queue.isnot(None))
+            .all()
+        )
+        for row in queue_rows:
+            queue = row.queue
+            ads = list(map(lambda el: AutoAdvertisementPage(el), queue))
+            await asyncio.gather(*[ad.get_page_text() for ad in ads])
+            # TODO: here process text in LLM and then check against all CarSearches with same car_id and send the ad to user
+
