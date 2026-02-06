62fb32a6a2f5bcf740fae9a525c8105e8e878702
kipariss1 <kpassudin@gmail.com>
2026-02-06 16:11:40 +0100

    TODO: finish bazos worker

--- queue_svc/ollama_api/ollama_client.py
+++ queue_svc/ollama_api/ollama_client.py
@@ -1,6 +1,22 @@
 import requests
 from pathlib import Path
-from queue_svc.bazos_api.auto_bazos_api import AutoAdvertisementPage
+from typing import TypedDict, Literal, Union
+from src.models.models import CarModel
+import re
+
+
+class NotValidCarAd(TypedDict):
+    is_car_sale: Literal[False]
+
+class ValidCarAd(TypedDict):
+    is_car_sale: Literal[True]
+    brand: str
+    model: str
+    engine: str
+    year: str
+    mileage: str
+
+CarSaleResult = Union[ValidCarAd, NotValidCarAd]
 
 
 class OllamaClient:
@@ -9,5 +25,25 @@ class OllamaClient:
         self.url = "http://ollama:11434"
         self.prompt_path = Path(__file__).parent / "prompts" / "ollama_prompt.txt"
 
-    def process(ad_text: str):
-        pass
+    def _generate_prompt(self, ad_text: str, car: CarModel) -> str:
+        mapping = {
+            "<CAR_BRAND>": car.manufacturer,
+            "<CAR_MODEL>": car.model,
+            "<AD_TEXT>": ad_text
+        }
+        with open(self.prompt_path, "r") as f:
+            template = f.read()
+        for k, v in mapping.items():
+            template = re.sub(k, v, template)
+        return template
+
+    def process(self, ad_text: str, car: CarModel) -> CarSaleResult:
+        prompt = self._generate_prompt(ad_text, car)
+        response = requests.post(
+            f"{self.url}/api/generate",
+            json={
+                "model": "gemma3:4b",
+                "prompt": prompt
+            }
+        )
+        return response.json()["response"]
--- queue_svc/worker/bazos_worker.py
+++ queue_svc/worker/bazos_worker.py
@@ -1,12 +1,14 @@
 from src.database_utils import db_handler
-from src.models.models import AdQueue
+from src.models.models import AdQueue, CarModel, CarSearch
 from queue_svc.bazos_api.auto_bazos_api import AutoAdvertisementPage
+from queue_svc.ollama_api.ollama_client import OllamaClient
 import asyncio
 
 class BazosWorker:
     
     def __init__(self):
         self.db = db_handler.get_db_connection()
+        self.ollama = OllamaClient()
 
     async def process_queue(self):
         queue_rows = (
@@ -16,8 +18,14 @@ class BazosWorker:
         )
         for row in queue_rows:
             queue = row.queue
+            car = self.db.query(CarModel).filter(CarModel.id == row.car_model_id)
+            searches = self.db.query(CarSearch).filter(CarSearch.car_model_id == row.car_model_id)
             ads = list(map(lambda el: AutoAdvertisementPage(el), queue))
             await asyncio.gather(*[ad.get_page_text() for ad in ads])
-            # TODO: here process text in LLM and then check against all CarSearches with same car_id and send the ad to user
+            for ad in ads:
+                res = self.ollama.process(ad_text=ad.text, car=car)
+                # TODO: here make comparison of every search and parsed app !BUT! it requires change of model CarSearch (TBD)
+
+
             
 
