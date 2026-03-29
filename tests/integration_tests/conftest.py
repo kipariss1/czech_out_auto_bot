4f3c9c52d22ac0475c57ac3d6f591556713161f4
Kirill Rassudikhin <kirill.rassudilhin@alteryx.com>
2026-03-29 21:37:15 +0200

    mocked validate token feature

--- tests/integration_tests/conftest.py
+++ tests/integration_tests/conftest.py
@@ -1,1 +1,13 @@
+import os
+
 from tests.pytest_fixtures.common import build_mock_db, build_mock_bazos
+
+
+os.environ.setdefault("BOT_TOKEN", "test-token")
+
+try:
+    import telebot.util
+
+    telebot.util.validate_token = lambda token: True
+except ModuleNotFoundError:
+    pass
