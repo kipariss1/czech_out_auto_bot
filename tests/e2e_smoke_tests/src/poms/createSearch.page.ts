944dc74c97bb9759a77c046666798ec579090c36
Kirill Rassudikhin <kirill.rassudilhin@alteryx.com>
2026-07-06 15:37:06 +0200

    added switches to disable non-required filters for creating search

--- tests/e2e_smoke_tests/src/poms/createSearch.page.ts
+++ tests/e2e_smoke_tests/src/poms/createSearch.page.ts
@@ -25,10 +25,14 @@ export class CreateSearchPage extends BasePage {
     public yearToInput: Locator;
     public milegeFromInput: Locator;
     public milegeToInput: Locator;
+    public mileageFilterSwitch: Locator;
     public priceFromInput: Locator;
     public priceToInput: Locator;
+    public priceFilterSwitch: Locator;
     public PSCinput: Locator;
+    public PSCFilterSwitch: Locator;
     public kmRangeFromPSCinput: Locator;
+    public kmRangeFromPSCFilterSwitch: Locator;
     public submitBtn: Locator;
 
     constructor(protected page: Page) {
@@ -41,11 +45,21 @@ export class CreateSearchPage extends BasePage {
         this.yearToInput = page.locator('#id-input-year-range-to');
         this.milegeFromInput = page.locator('#id-input-mileage-range-from');
         this.milegeToInput = page.locator('#id-input-mileage-range-to');
+        this.mileageFilterSwitch = page.locator('#id-enable-mileage-filter');
         this.priceFromInput = page.locator('#id-input-price-range-from');
         this.priceToInput = page.locator('#id-input-price-range-to');
+        this.priceFilterSwitch = page.locator('#id-enable-price-filter');
         this.PSCinput = page.locator('#id-psc-code');
+        this.PSCFilterSwitch = page.locator('#id-enable-psc-filter');
         this.kmRangeFromPSCinput = page.locator('#id-psc-km-range');
+        this.kmRangeFromPSCFilterSwitch = page.locator('#id-enable-psc-km-filter');
         this.submitBtn = page.getByTestId("submit-btn");
+    }
+
+    private async enableOptionalFilter(filterSwitch: Locator) {
+        if (!(await filterSwitch.isChecked())) {
+            await filterSwitch.check();
+        }
     }
 
     async createNewSearch(data: SearchFormInputs) {
@@ -66,11 +80,17 @@ export class CreateSearchPage extends BasePage {
         }
         await this.yearFromInput.fill(data.yearFromInput.toString());
         await this.yearToInput.fill(data.yearToInput.toString());
+        if (data.mileageFrom !== undefined || data.mileageTo !== undefined) {
+            await this.enableOptionalFilter(this.mileageFilterSwitch);
+        }
         if (data.mileageFrom !== undefined) {
             await this.milegeFromInput.fill(data.mileageFrom.toString());
         }
         if (data.mileageTo !== undefined) {
             await this.milegeToInput.fill(data.mileageTo.toString());
+        }
+        if (data.priceFrom !== undefined || data.priceTo !== undefined) {
+            await this.enableOptionalFilter(this.priceFilterSwitch);
         }
         if (data.priceFrom !== undefined) {
             await this.priceFromInput.fill(data.priceFrom.toString());
@@ -79,9 +99,11 @@ export class CreateSearchPage extends BasePage {
             await this.priceToInput.fill(data.priceTo.toString());
         }
         if (data.pscCode !== undefined) {
+            await this.enableOptionalFilter(this.PSCFilterSwitch);
             await this.PSCinput.fill(data.pscCode);
         }
         if (data.kmRangeFromPSC !== undefined) {
+            await this.enableOptionalFilter(this.kmRangeFromPSCFilterSwitch);
             await this.kmRangeFromPSCinput.fill(data.kmRangeFromPSC.toString());
         }
         await this.submitBtn.click();
--- web_app/static/js/create_sliders__create_search_form.js
+++ web_app/static/js/create_sliders__create_search_form.js
@@ -8,6 +8,12 @@ var input_mileage_to = document.getElementById('id-input-mileage-range-to');
 var slider_price = document.getElementById('id-slider-price');
 var input_price_from = document.getElementById('id-input-price-range-from');
 var input_price_to = document.getElementById('id-input-price-range-to');
+var mileage_filter_switch = document.getElementById('id-enable-mileage-filter');
+var price_filter_switch = document.getElementById('id-enable-price-filter');
+var psc_filter_switch = document.getElementById('id-enable-psc-filter');
+var psc_km_filter_switch = document.getElementById('id-enable-psc-km-filter');
+var psc_input = document.getElementById('id-psc-code');
+var psc_km_input = document.getElementById('id-psc-km-range');
 
 var list_sliders = [slider_year, slider_mileage, slider_price];
 var list_from = [input_year_from, input_mileage_from, input_price_from];
@@ -15,8 +21,20 @@ var list_to = [input_year_to, input_mileage_to, input_price_to];
 var list_show = [[1990, 2010], [50000, 200000], [50000, 100000]];
 var list_range = [[1950, currentYear], [0, 500000], [0, 1000000]];
 var list_optional = [false, true, true];
+var list_switches = [null, mileage_filter_switch, price_filter_switch];
 
-function create_slider(slider, show, range, step, input_from, input_to, optional) {
+function clear_input(input) {
+    input.value = "";
+}
+
+function set_input_enabled(input, enabled) {
+    input.disabled = !enabled;
+    if (!enabled) {
+        clear_input(input);
+    }
+}
+
+function create_slider(slider, show, range, step, input_from, input_to, optional, optional_switch) {
     noUiSlider.create(slider, {
         start: [show[0], show[1]],
         connect: true,
@@ -35,15 +53,8 @@ function create_slider(slider, show, range, step, input_from, input_to, optional
         }
     });
 
-    var sync_inputs = !optional;
-    if (optional) {
-        setTimeout(function() {
-            sync_inputs = true;
-        }, 0);
-    }
-
     slider.noUiSlider.on("update", function (values, handle) {
-        if (!sync_inputs) {
+        if (optional && !optional_switch.checked) {
             return;
         }
         if (handle === 0) {
@@ -53,23 +64,74 @@ function create_slider(slider, show, range, step, input_from, input_to, optional
         }
     });
     input_from.addEventListener('change', function() {
+        if (optional && !optional_switch.checked) {
+            return;
+        }
         if (optional && this.value === "") {
             return;
         }
         slider.noUiSlider.set([this.value, null])
     });
     input_to.addEventListener('change', function() {
+        if (optional && !optional_switch.checked) {
+            return;
+        }
         if (optional && this.value === "") {
             return;
         }
         slider.noUiSlider.set([null, this.value])
     });
+
     if (optional) {
-        input_from.value = "";
-        input_to.value = "";
+        optional_switch.checked = false;
+        set_optional_slider_state(
+            slider,
+            input_from,
+            input_to,
+            optional_switch.checked
+        );
+        optional_switch.addEventListener('change', function() {
+            set_optional_slider_state(slider, input_from, input_to, this.checked);
+        });
     }
 }
 
+function set_optional_slider_state(slider, input_from, input_to, enabled) {
+    if (enabled) {
+        slider.noUiSlider.enable();
+        input_from.disabled = false;
+        input_to.disabled = false;
+        var values = slider.noUiSlider.get();
+        input_from.value = Math.round(values[0]);
+        input_to.value = Math.round(values[1]);
+        return;
+    }
+
+    slider.noUiSlider.disable();
+    set_input_enabled(input_from, false);
+    set_input_enabled(input_to, false);
+}
+
+function create_optional_input(input, optional_switch) {
+    optional_switch.checked = false;
+    set_input_enabled(input, optional_switch.checked);
+    optional_switch.addEventListener('change', function() {
+        set_input_enabled(input, this.checked);
+    });
+}
+
 list_sliders.forEach((slider, i) => {
-    create_slider(slider, list_show[i], list_range[i], 1, list_from[i], list_to[i], list_optional[i]);
+    create_slider(
+        slider,
+        list_show[i],
+        list_range[i],
+        1,
+        list_from[i],
+        list_to[i],
+        list_optional[i],
+        list_switches[i]
+    );
 })
+
+create_optional_input(psc_input, psc_filter_switch);
+create_optional_input(psc_km_input, psc_km_filter_switch);
